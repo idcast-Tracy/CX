dabc185994f83a22d1fb02c4e6aee91a2a1d7a0a
idcast-Tracy <138656256+idcast-Tracy@users.noreply.github.com>
2024-04-06 11:11:21 +0800

    一叶知秋.py

--- 一叶知秋.py
+++ 一叶知秋.py
@@ -22,155 +22,178 @@ footer {visibility:hidden;}
 st.markdown(hide_menu,unsafe_allow_html=True)
 # st.balloons()
 
+
 with st.sidebar:
-    choose = option_menu("管理层", ["数据监控与分析", "数据安全与运维保障服务", "AI启动项","跨平台同步学习模块"],
-                         icons=['house', 'book-half', 'bar-chart', "boombox-fill"],
-                         menu_icon="bullseye", default_index=0)
+    choose = option_menu("管理层", ["数据上传", "用户行为数据", "系统性能评估",
+                                    "数据备份", "安全审计", "系统监控",
+                                    "创造力", "平衡", "保守",
+                                    "手机端", "平板端", "电脑端"],
+                         icons=['cloud-upload', 'person lines fill', 'app-indicator', "boombox-fill",
+                                'list-task', 'gear', 'bar-chart', "boombox-fill",
+                                'house', 'file-slides', 'bar-chart', "cast"],
+                         menu_icon="list", default_index=0,
+                         styles={
+        "container": {"padding": "5!important", "background-color": "#dceef8"}, # 整体颜色
+        "icon": {"color": "#0e427a", "font-size": "25px"}, # 图标颜色和大小设定
+        "nav-link": {"font-size": "25px", "text-align": "left", "margin":"0px", "--hover-color": "#f1f1f2"}, # 图标旁文字大小和点击时颜色设定
+        "nav-link-selected": {"background-color": "#4FC8DD"} # 点击后背景色
+    })
 
 ## ==================================  数据监控与分析  ==========================================
-if choose == "数据监控与分析":
-    with st.sidebar:
-        choose = option_menu("数据监控与分析", ["数据上传", "用户行为数据", "系统性能评估"],
-                             icons=['house', 'book-half', 'bar-chart', "boombox-fill"],
-                             menu_icon="house", default_index=0)
+if choose == "数据上传":
+    ## file_uploader【文件上传】
+    uploaded_files = st.file_uploader("选择需上传的文件(包括试卷库、作业库、题库、PPT等)：", accept_multiple_files=True, type=["csv"])
+    for uploaded_file in uploaded_files:
+        df = pd.read_csv(uploaded_file,encoding='gbk')
+        st.write("文件名:", uploaded_file.name)
+        latest_iteration = st.empty() ##  显示进度
+        bar = st.progress(0)
+        for i in range(100): # Update the progress bar with each iteration.
+            latest_iteration.text(f'Iteration {i + 1}')
+            bar.progress(i + 1)
+            time.sleep(0.00000000001)
+        st.success('数据导入成功！请检查并核对数据的完整性和真实性。')
+        st.dataframe(df)
 
-    if choose == "数据上传":
-        ## file_uploader【文件上传】
-        uploaded_files = st.file_uploader("请选择文件(试卷库、作业库、题库、PPT等)：", accept_multiple_files=True, type=["csv"])
-        for uploaded_file in uploaded_files:
-            df = pd.read_csv(uploaded_file,encoding='gbk')
-            st.write("文件名:", uploaded_file.name)
-            latest_iteration = st.empty() ##  显示进度
-            bar = st.progress(0)
-            for i in range(100): # Update the progress bar with each iteration.
-                latest_iteration.text(f'Iteration {i + 1}')
-                bar.progress(i + 1)
-                time.sleep(0.00000000001)
-            st.success('数据导入成功，请检查数据完整性和真实性。')
-            st.dataframe(df)
+elif choose == "用户行为数据":
+    selecte1 = option_menu(None, ["用户分布地区-条形图", "用户特征分布-饼图", "用户活跃度-折线图"],
+                          icons=["bar-chart-fill", "pie-chart-fill", "graph-up"],
+                          menu_icon="cast", default_index=0, orientation="horizontal",
+                           styles={
+                               "container": {"padding": "5!important", "background-color": "#dceef8"},  # 整体颜色
+                               "icon": {"color": "#0e427a", "font-size": "25px"},  # 图标颜色和大小设定
+                               "nav-link": {"font-size": "25px", "text-align": "left", "margin": "0px", "--hover-color": "#f1f1f2"},  # 图标旁文字大小和点击时颜色设定
+                               "nav-link-selected": {"background-color": "#4FC8DD"}}
+                           )
+    if selecte1 == "用户分布地区-条形图":
+        st.info('条形图直观地展示了用户在不同地区的分布情况，使得数据的对比一目了然。通过不同条形的长度，可以迅速识别出用户的主要聚集地和相对较少的地区，有助于企业制定针对性的市场策略。')
+        st.image("用户分布地区.png",use_column_width=True)
 
-    elif choose == "用户行为数据":
-        selecte = option_menu(None, ["用户分布地区", "用户特征分布", "用户活跃度"],
-                              icons=["bar-chart-fill", "pie-chart-fill", "graph-up"],
-                              menu_icon="cast", default_index=0, orientation="horizontal")
-        if selecte == "用户分布地区":
-            st.image("用户分布地区.png")
+    elif selecte1 == "用户特征分布-饼图":
+        st.info('饼图以圆形为基础，通过划分不同的扇形区域来展示用户特征的分布情况。这种图表形式使得数据的占比关系非常清晰，便于快速了解各类用户特征的比例，为企业的用户分析和精准营销提供有力支持。')
+        st.image("用户特征分布.png",use_column_width=True)
 
-        elif selecte == "用户特征分布":
-            st.image("用户特征分布.png")
-
-        elif selecte == "用户活跃度":
-            st.image("用户活跃度.png")
+    elif selecte1 == "用户活跃度-折线图":
+        st.info('折线图能够清晰地展示用户活跃度随时间的变化趋势，通过连线的起伏可以直观地看出活跃度的增减情况。这种图表形式有助于企业把握用户活跃度的变化规律，从而及时调整运营策略，提升用户留存和活跃度。')
+        st.image("用户活跃度.png",use_column_width=True)
 
 
-    elif choose == "系统性能评估":
-        selecte = option_menu(None, ["推荐效果评估","问答系统性能评估"],icons=["pie-chart-fill","graph-up"],
-                              menu_icon="cast", default_index=0, orientation="horizontal")
-        if selecte == "推荐效果评估":
-            st.info('一个用于分析和展示系统推荐性能的核心功能，这有助于管理层精准掌握系统性能，为决策提供有力支持，推动系统持续优化发展')
-            st.image("推荐效果评估.png")
+elif choose == "系统性能评估":
+    selecte2 = option_menu(None, ["推荐效果评估","问答系统性能评估曲线"],icons=["pie-chart-fill","graph-up"],
+                          menu_icon="cast", default_index=0, orientation="horizontal",
+                           styles={
+                               "container": {"padding": "5!important", "background-color": "#dceef8"},  # 整体颜色
+                               "icon": {"color": "#0e427a", "font-size": "25px"},  # 图标颜色和大小设定
+                               "nav-link": {"font-size": "25px", "text-align": "left", "margin": "0px", "--hover-color": "#f1f1f2"},  # 图标旁文字大小和点击时颜色设定
+                               "nav-link-selected": {"background-color": "#4FC8DD"}}
+                          )
+    if selecte2 == "推荐效果评估":
+        st.info('一个用于分析和展示系统推荐性能的核心功能，这有助于管理层精准掌握系统性能，为决策提供有力支持，推动系统持续优化发展')
+        st.image("推荐效果评估.png",use_column_width=True)
 
-        elif selecte == "问答系统性能评估":
-            st.info('智能问答系统性能检测：确保高效、准确的问答体验。让学习更便捷！')
-            st.image("问答系统性能评估.png")
+    elif selecte2 == "问答系统性能评估曲线":
+        st.info('智能问答系统性能检测：确保高效、准确的问答体验。让学习更便捷！')
+        st.image("问答系统性能评估.png",use_column_width=True)
 
 
 ## ==================================  数据安全与运维保障服务  ==========================================
-elif choose == "数据安全与运维保障服务":
-    with st.sidebar:
-        choose = option_menu("数据安全与运维保障服务", ["数据备份", "安全审计", "系统监控"],
-                             icons=['house', 'bar-chart', "boombox-fill"],
-                             menu_icon="book-half", default_index=0)
+if choose == "数据备份":
+    selecte3 = option_menu(None, ["配置选项", "高级选项", "更新备份数据"],icons=["bar-chart-fill", "pie-chart-fill", "graph-up"],
+                          menu_icon="cast", default_index=0, orientation="horizontal",
+                           styles={
+                               "container": {"padding": "5!important", "background-color": "#dceef8"},  # 整体颜色
+                               "icon": {"color": "#0e427a", "font-size": "25px"},  # 图标颜色和大小设定
+                               "nav-link": {"font-size": "25px", "text-align": "left", "margin": "0px", "--hover-color": "#f1f1f2"},  # 图标旁文字大小和点击时颜色设定
+                               "nav-link-selected": {"background-color": "#4FC8DD"}}
+                          )
+    if selecte3 == "配置选项":
+        option2 = st.selectbox("数据源", ("数据库服务器", "文件服务器", "应用服务器"))
+        option3 = st.selectbox("备份频率", ("每小时", "每日", "每周", "每月"))
+        txt1 = st.text_area('请手动输入存储位置')
+        txt2 = st.text_area('请手动输入备份任务名称')
+    elif selecte3 == "高级选项":
+        option4 = st.selectbox("备份压缩", ("启用备份数据压缩","不启用"))
+        option5 = st.selectbox("增量备份", ("启用增量备份","不启用"))
+        option6 = st.selectbox("备份日志级别", ("详细",'标准',"简单"))
+        txt3 = st.date_input("过期备份保留时间", datetime.date(2024, 4, 8))
+    elif selecte3 == "更新备份数据":
+        df = pd.DataFrame({'任务名称': ['任务1', '任务2', '任务3', '任务4'],
+                           '数据源': ['数据库服务器', '文件服务器', '应用服务器', '数据库服务器'],
+                           '备份频率': ['每小时', '每日', '每周', '每月'],
+                           '备份状态': ['已完成', '进行中', '未开始', '已完成'],
+                           '上次备份时间': ['2024-04-08 08:00', '2024-04-07 18:30', '-', '2024-03-30 22:00'],
+                           '下次备份时间': ['2024-04-09 08:00', '2024-04-10 18:30', '2024-04-06 12:00', '2024-04-03 22:00']})
+        df
 
-    if choose == "数据备份":
-        selecte = option_menu(None, ["配置选项", "高级选项", "更新备份数据"],icons=["bar-chart-fill", "pie-chart-fill", "graph-up"],
-                              menu_icon="cast", default_index=0, orientation="horizontal")
-        if selecte == "配置选项":
-            option2 = st.selectbox("数据源", ("数据库服务器", "文件服务器", "应用服务器"))
-            option3 = st.selectbox("备份频率", ("每小时", "每日", "每周", "每月"))
-            txt1 = st.text_area('请输入存储位置')
-            txt2 = st.text_area('请输入备份任务名称')
-        elif selecte == "高级选项":
-            option4 = st.selectbox("备份压缩", ("启用备份数据压缩","不启用"))
-            option5 = st.selectbox("增量备份", ("启用增量备份","不启用"))
-            option6 = st.selectbox("备份日志级别", ("详细",'标准',"简单"))
-            txt3 = st.date_input("过期备份保留时间", datetime.date(2024, 4, 8))
-        elif selecte == "更新备份数据":
-            df = pd.DataFrame({'任务名称': ['任务1', '任务2', '任务3', '任务4'],
-                               '数据源': ['数据库服务器', '文件服务器', '应用服务器', '数据库服务器'],
-                               '备份频率': ['每小时', '每日', '每周', '每月'],
-                               '备份状态': ['已完成', '进行中', '未开始', '已完成'],
-                               '上次备份时间': ['2024-04-08 08:00', '2024-04-07 18:30', '-', '2024-03-30 22:00'],
-                               '下次备份时间': ['2024-04-09 08:00', '2024-04-10 18:30', '2024-04-06 12:00', '2024-04-03 22:00']})
-            df
+if choose == "安全审计":
+    selecte4 = option_menu(None, ["审计日志表","审计日志详细界面"],icons=["pie-chart-fill","graph-up"],
+                          menu_icon="cast", default_index=0, orientation="horizontal",
+                           styles={
+                               "container": {"padding": "5!important", "background-color": "#dceef8"},  # 整体颜色
+                               "icon": {"color": "#0e427a", "font-size": "25px"},  # 图标颜色和大小设定
+                               "nav-link": {"font-size": "25px", "text-align": "left", "margin": "0px", "--hover-color": "#f1f1f2"},  # 图标旁文字大小和点击时颜色设定
+                               "nav-link-selected": {"background-color": "#4FC8DD"}}
+                          )
+    if selecte4 == "审计日志表":
+        st.info("审计日志表以直观、简洁的方式展示了审计数据的统计结果，用户可以迅速了解审计活动的整体情况，如用户的登录次数、登录时间等。这种图表形式有助于用户快速发现异常或风险点，为决策提供有力支持。")
+        st.image("安全审计1.png",use_column_width=True)
 
-    if choose == "安全审计":
-        selecte = option_menu(None, ["审计日志图表","审计日志详细界面"],icons=["pie-chart-fill","graph-up"],
-                              menu_icon="cast", default_index=0, orientation="horizontal")
-        if selecte == "审计日志图表":
-            st.image("安全审计1.png")
+    elif selecte4 == "审计日志详细界面":
+        st.info("审计日志详细界面提供了审计事件的详细信息，包括事件发生的时间、操作人、操作内容等。这一界面设计使得用户可以深入了解每个审计事件的具体情况，为进一步的调查和分析提供了丰富的数据支持。")
+        st.image("安全审计2.png",use_column_width=True)
 
-        elif selecte == "审计日志详细界面":
-            st.image("安全审计2.png")
-
-    if choose == "系统监控":
-        selecte = option_menu(None, ["安全监控","服务监控","日志监控","性能监控"],icons=["pie-chart-fill","graph-up"],
-                              menu_icon="cast", default_index=0, orientation="horizontal")
-        if selecte == "安全监控":
-            st.image("安全监控.png")
-        elif selecte == "服务监控":
-            st.image("服务监控.png")
-        elif selecte == "日志监控":
-            st.image("日志监控.png")
-        elif selecte == "性能监控":
-            st.image("性能监控.png")
+if choose == "系统监控":
+    selecte5 = option_menu(None, ["安全监控","服务监控","日志监控","性能监控"],icons=["pie-chart-fill","graph-up"],
+                          menu_icon="cast", default_index=0, orientation="horizontal",
+                           styles={
+                               "container": {"padding": "5!important", "background-color": "#dceef8"},  # 整体颜色
+                               "icon": {"color": "#0e427a", "font-size": "25px"},  # 图标颜色和大小设定
+                               "nav-link": {"font-size": "25px", "text-align": "left", "margin": "0px", "--hover-color": "#f1f1f2"},  # 图标旁文字大小和点击时颜色设定
+                               "nav-link-selected": {"background-color": "#4FC8DD"}}
+                          )
+    if selecte5 == "安全监控":
+        st.info("安全监控是保障系统稳定运行的重要环节，通过实时监控网络流量、用户行为等，及时发现并阻止潜在的安全威胁。它能有效预防数据泄露、黑客攻击等风险，确保企业信息安全。")
+        st.image("安全监控.png",use_column_width=True)
+    elif selecte5 == "服务监控":
+        st.info("服务监控是对企业关键业务服务进行持续追踪和管理的过程，确保服务的可用性、稳定性和性能。通过监控，企业可以迅速响应服务中断或性能下降等问题，保障用户体验和业务连续性。")
+        st.image("服务监控.png",use_column_width=True)
+    elif selecte5 == "日志监控":
+        st.info("日志监控是对系统、应用等产生的日志信息进行收集、分析和管理的过程。通过监控日志，企业可以了解系统的运行状况、发现潜在问题，并为故障排查和性能优化提供有力支持。")
+        st.image("日志监控.png",use_column_width=True)
+    elif selecte5 == "性能监控":
+        st.info("性能监控是对系统、应用等性能指标的持续测量和评估。它帮助企业了解系统的运行效率、资源使用情况等，为性能优化和容量规划提供依据。通过性能监控，企业可以确保系统在高负载下仍能保持稳定运行。")
+        st.image("性能监控.png",use_column_width=True)
 
 
 ## ==================================  AI启动项  ==========================================
-elif choose == "AI启动项":
-    with st.sidebar:
-        choose = option_menu("AI启动项", ["创造力", "平衡", "保守"],
-                             icons=['house', 'book-half', "boombox-fill"],
-                             menu_icon="bar-chart", default_index=0)
-    # 结合Plotly来添加柱状图、饼图和折线图
-    # 将图表分为三种
-    selecte = option_menu(None, ["柱状图", "饼图", "折线图"],
-                          icons=["bar-chart-fill", "pie-chart-fill", "graph-up"],
-                          menu_icon="cast", default_index=0, orientation="horizontal")
-    if selecte == "柱状图":
-        # px.data.tips()`是Plotly Express提供的一个示例数据集，其中包含有关餐厅小费的数据它是一个DataFrame对象
-        data_bar = px.data.tips()
-        fig_bar = px.bar(data_bar, x='day', y='total_bill', color='sex')
-        st.plotly_chart(fig_bar)
+if choose == "创造力":
+    # px.data.tips()`是Plotly Express提供的一个示例数据集，其中包含有关餐厅小费的数据它是一个DataFrame对象
+    data_bar = px.data.tips()
+    fig_bar = px.bar(data_bar, x='day', y='total_bill', color='sex')
+    st.plotly_chart(fig_bar)
 
-    elif selecte == "饼图":
-        data_pie = px.data.tips()
-        fig_pie = px.pie(data_pie, names='day')
-        st.plotly_chart(fig_pie)
+elif choose == "平衡":
+    data_pie = px.data.tips()
+    fig_pie = px.pie(data_pie, names='day')
+    st.plotly_chart(fig_pie)
 
-    elif selecte == "折线图":
-        data_line = px.data.gapminder().query("country=='China'")
-        fig_line = px.line(data_line, x='year', y='pop')
-        st.plotly_chart(fig_line)
+elif choose == "保守":
+    data_line = px.data.gapminder().query("country=='China'")
+    fig_line = px.line(data_line, x='year', y='pop')
+    st.plotly_chart(fig_line)
 
 
 ## ==================================  跨平台同步学习模块  ==========================================
-elif choose == "跨平台同步学习模块":
-    with st.sidebar:
-        choose = option_menu("跨平台同步学习模块", ["手机端", "平板端", "电脑端"],
-                             icons=['house', 'book-half', 'bar-chart'],
-                             menu_icon="boombox-fill", default_index=0)
-    selecte = option_menu(None, ["音乐", "视频"],
-                          icons=["file-music-fill", "badge-vo-fill"],
-                          menu_icon="cast", default_index=0, orientation="horizontal")
-    if selecte == "音乐":
-        st.write("我的梦")
-        st.audio("Dream It Possible.mp3")
-        st.write("离别开出花")
-        st.audio("离别开出花.mp3")
-        st.write("明天，你好")
-        st.audio("明天,你好.mp3")
+if choose == "手机端":
+    st.write("我的梦")
+    st.audio("Dream It Possible.mp3")
 
-    elif selecte == "视频":
-        # st.video("S4.原型演示(视频)-赛题08-可视化AI模型平台-视界AI.mp4")
-        st.write('傻逼')
+elif choose == "平板端":
+    st.write("离别开出花")
+    st.audio("离别开出花.mp3")
+
+elif choose == "电脑端":
+    st.write("明天，你好")
+    st.audio("明天,你好.mp3")
+
+
